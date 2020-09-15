db7c5857d750425e064f43183df64b91f7a1260a
ksdkamesh99 <ksdkamesh99@gmail.com>
2020-09-15 16:30:55 +0530

    Text Preprocessing and nltk libraries added

--- StackQuestioner/views.py
+++ StackQuestioner/views.py
@@ -5,6 +5,12 @@ from django.http import HttpResponse
 import tensorflow,keras
 import pickle
 import tensorflow
+import re
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('punkt')
+nltk.download('stopwords')
 
 lang=['R', 'java', 'javascript', 'php', 'python']
 
@@ -15,11 +21,16 @@ def home(request):
 
 def predict(request):
     ques=request.POST['ques']
+    sent=re.sub('[^a-zA-Z]',' ',ques)
+    sent_lower=sent.lower()
+    words_split=sent_lower.split(' ')
+    words=[x for x in words_split if x not in stopwords.words('english')]
+    sentence=' '.join(words)
 
 
     model=tensorflow.keras.models.load_model('model.h5')
     tokenizer=pickle.load(open('tokenizer.pkl','rb+'))
-    seq=tokenizer.texts_to_sequences([ques])
+    seq=tokenizer.texts_to_sequences([sentence])
     padseq=tensorflow.keras.preprocessing.sequence.pad_sequences(seq,maxlen=26,padding='post')
     result=model.predict(padseq)
     result_lang=np.argmax(result[0])
